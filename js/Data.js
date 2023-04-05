60079e734c3e24c10d6262f36749b41eabcc0331
andres trilli <apuello9@cuc.edu.co>
2023-04-04 19:18:34 -0500

    cambios

--- js/Data.js
+++ js/Data.js
@@ -2,7 +2,6 @@ var tabla= [
     {
         id:'1',
         nombre:'aaa',
-        usuario:'usuarioo',
         apellidos:'apellidoos',
         correo:'correoo',
         contraseña:'12345',
@@ -14,7 +13,6 @@ var tabla= [
     {
         id:'1',
         nombre:'bbbb',
-        usuario:'ccccccc',
         apellidos:'apellidoos',
         correo:'correoo',
         contraseña:'12345',
@@ -26,7 +24,6 @@ var tabla= [
     {
         id:'1',
         nombre:'bbb',
-        usuario:'bbbbbbb',
         apellidos:'apellidoos',
         correo:'correoo',
         contraseña:'12345',
@@ -38,16 +35,16 @@ var tabla= [
 ];
 
 let contenidoTabla=document.getElementById('tabla-usuario')
+
 function Listar() {
     let listado="";
     for (let i = 0; i < tabla.length; i++) {
-        listado+=`<tr><th scope="row">'${(i+1)}</th><td>${tabla[i].nombre}</td><td>${tabla[i].usuario}</td><td>${tabla[i].apellidos}</td><td>${tabla[i].correo}</td><td>${tabla[i].contraseña}</td><td>${tabla[i].cedula}</td><td>${tabla[i].telefono}</td><td>${tabla[i].ciudad}</td><td>${tabla[i].nacimiento}</td></tr>`
+        listado+=`<tr><th scope="row">'${(i+1)}</th><td>${tabla[i].nombre}</td><td>${tabla[i].apellidos}</td><td>${tabla[i].correo}</td><td>${tabla[i].contraseña}</td><td>${tabla[i].cedula}</td><td>${tabla[i].telefono}</td><td>${tabla[i].ciudad}</td><td>${tabla[i].nacimiento}</td></tr>`
     }
     contenidoTabla.innerHTML=listado
 }
 
 function guardarData() {
-    let usuario=document.getElementById('usuario').value;
     let nombre=document.getElementById('nombre').value;
     let apellidos=document.getElementById('apellidos').value;
     let correo=document.getElementById('correo').value;
@@ -56,10 +53,7 @@ function guardarData() {
     let telefono=document.getElementById('telefono').value;
     let ciudad=document.getElementById('ciudad').value;
     let nacimiento=document.getElementById('nacimiento').value;
-    if (!usuario) {
-        alert('Ingrese la identificacion')
-        return
-    }
+
     if(!nombre) {
         alert('Ingrese el nombre')
         return
@@ -69,34 +63,36 @@ function guardarData() {
         return
     }
     if (!correo) {
-        alert('Ingrese la dirección')
+        alert('Ingrese la correo')
         return
     }
     if(!contraseña) {
-        alert('Ingrese el correo')
+        alert('Ingrese el contraseña')
         return
     }
     if (!cedula) {
-        alert('Ingrese el telefono')
+        alert('Ingrese el cedula')
         return
     }
     if (!telefono) {
-        alert('Ingrese su pais')
+        alert('Ingrese su telefono')
         return
     }
     if (!ciudad) {
-        alert('Ingrese su fecha de nacimiento')
+        alert('Ingrese su ciudad')
         return
     }
     if (!nacimiento) {
         alert('Ingrese su fecha de nacimiento')
         return
     }
-    contenidoTabla.innerHTML+=`<tr><th scope="row">'${(i+1)}</th><td>${nombre}</td><td>${usuario}</td><td>${apellidos}</td><td>${correo}</td><td>${contraseña}</td><td>${cedula}</td><td>${telefono}</td><td>${ciudad}</td><td>${nacimiento}</td></tr>`
+  //  contenidoTabla.innerHTML+=`<tr><th scope="row">'${(i+1)}</th><td>${nombre}</td><td>${apellidos}</td><td>${correo}</td><td>${contraseña}</td><td>${cedula}</td><td>${telefono}</td><td>${ciudad}</td><td>${nacimiento}</td></tr>`
     
-    let nuevo={nombre:nombre,usuario:usuario,apellidos:apellidos,correo:correo,contraseña:contraseña,cedula:cedula,telefono:telefono,ciudad:ciudad,nacimiento:nacimiento}
+    let nuevo={nombre:nombre,apellidos:apellidos,correo:correo,contraseña:contraseña,cedula:cedula,telefono:telefono,ciudad:ciudad,nacimiento:nacimiento}
      tabla.push(nuevo)
      alert('registrado')
-
+     Listar()
+     event.preventDefault()
     // '<tr><th scope="row">'+(i+1)+'</th><td>'+nombre+'</td><td>'+usuario+'</td><td>'+apellidos+'</td><td>'+correo+'</td><td>'+contraseña+'</td><td>'+cedula+'</td><td>'+telefono+'</td><td>'+ciudad+'</td><td>'+fechaNacimiento+'</td></tr>'
+
 }
